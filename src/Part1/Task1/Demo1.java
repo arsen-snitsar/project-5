6e9e08df8099671af27a92785f0ea4b87a996912
admin <arsen.snitsar@auk.edu.ua>
2023-09-11 21:02:52 +0300

    Completed first task

--- src/Part1/Task1/Demo1.java
+++ src/Part1/Task1/Demo1.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Demo1 {
 
+    public static byte askMonth(){
+        System.out.println("Enter month (1-12): ");
+        return keyboard.nextByte();
+    }
+
     public static String getMonth(byte month){
         switch (month) {
             case 1 -> {
@@ -49,19 +54,48 @@ public class Demo1 {
         }
     }
 
-    public static byte askMonth(){
-        System.out.println("Enter month (1-12): ");
-        return keyboard.nextByte();
+    public static float askFloat(int whichNumber){
+        switch (whichNumber) {
+            case 1 -> System.out.println("Please, enter first number:");
+            case 2 -> System.out.println("Please, enter second number:");
+            case 3 -> System.out.println("Please, enter third number: ");
+        }
+        return keyboard.nextFloat();
     }
 
     public static float averageOfThree(float numberOne, float numberTwo, float numberThree){
         return (numberOne + numberTwo + numberThree) / 3;
     }
 
+    public static boolean askBoolean(int whichBoolean){
+        switch (whichBoolean) {
+            case 1 -> System.out.println("Please, enter first boolean:");
+            case 2 -> System.out.println("Please, enter second boolean: ");
+        }
+        return keyboard.nextBoolean();
+    }
+
+    public static boolean doAnd(boolean firstBoolean, boolean secondBoolean){
+        if (firstBoolean && secondBoolean)
+            return true;
+        return false;
+    }
+
     public static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(getMonth(askMonth()));
 
+        //System.out.println(getMonth(askMonth()));
+
+        /*float numberOne, numberTwo, numberThree;
+        numberOne = askFloat(1);
+        numberTwo = askFloat(2);
+        numberThree = askFloat(3);
+        System.out.println(averageOfThree(numberOne, numberTwo, numberThree));*/
+
+        boolean firstBoolean, secondBoolean;
+        firstBoolean = askBoolean(1);
+        secondBoolean = askBoolean(2);
+        System.out.println(doAnd(firstBoolean, secondBoolean));
     }
 }

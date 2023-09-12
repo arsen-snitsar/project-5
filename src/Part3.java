75a2b6d826468e7f3b6adffe455b4c6580b452dc
admin <arsen.snitsar@auk.edu.ua>
2023-09-12 09:02:57 +0300

    Made Calculator (somewhat)

--- src/Part3.java
+++ src/Part3.java
@@ -6,28 +6,75 @@ public class Part3 {
 
     public static Scanner keyboard = new Scanner(System.in);
 
-    public static double addition(double firstNumber, double secondNumber){
+    public static double add(double firstNumber, double secondNumber){
         return firstNumber + secondNumber;
     }
 
-    public static double substraction(double firstNumber, double secondNumber){
+    public static double substract(double firstNumber, double secondNumber){
         return firstNumber - secondNumber;
     }
 
-    public static double multiplication(double firstNumber, double secondNumber){
+    public static double multiplicate(double firstNumber, double secondNumber){
         return firstNumber * secondNumber;
     }
 
-    public static double division(double firstNumber, double secondNumber){
+    public static double divide(double firstNumber, double secondNumber){
         return firstNumber / secondNumber;
+    }
+
+    public static double enterNumber(boolean isFirstNumber){
+        if (isFirstNumber)
+            System.out.print("Please, enter the first number: ");
+        else
+            System.out.print("Please, enter the second number: ");
+        double enteredNumber = keyboard.nextDouble();
+        keyboard.nextLine();
+        return enteredNumber;
+    }
+
+    public static String enterOperator(){
+        System.out.print("Please, enter the operator: ");
+        return keyboard.nextLine();
+    }
+
+    public static double result(double firstOperand, String operator, double secondOperand){
+        switch (operator) {
+            case "+" -> {
+                return add(firstOperand, secondOperand);
+            }
+            case "-" -> {
+                return substract(firstOperand, secondOperand);
+            }
+            case "*" -> {
+                return multiplicate(firstOperand, secondOperand);
+            }
+            case "/" -> {
+                return divide(firstOperand, secondOperand);
+            }
+            default -> {
+                return -1;
+            }
+        }
+    }
+
+    public static boolean askIfContinue(){
+        System.out.println("\nContinue? (y/n)");
+        String shouldContinue = keyboard.nextLine();
+        return shouldContinue.equals("Y") || shouldContinue.equals("y");
     }
 
     public static void main(String[] args) {
 
         double firstOperand, secondOperand;
-        String operator;
+        String operator, shouldContinue;
 
+        do {
+            firstOperand = enterNumber(true);
+            operator = enterOperator();
+            secondOperand = enterNumber(false);
 
+            System.out.printf("%.2f", result(firstOperand, operator, secondOperand));
+        }while (askIfContinue());
 
     }
 }
